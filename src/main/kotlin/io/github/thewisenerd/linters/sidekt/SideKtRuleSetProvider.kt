2fe076c3ca2cc9b55ca603510dc41b2d6d6374c8
thewisenerd <thewisenerd@protonmail.com>
2021-04-17 19:54:13 +0530

    fix plugin

--- src/main/kotlin/io/github/thewisenerd/linters/sidekt/SideKtRuleSetProvider.kt
+++ src/main/kotlin/io/github/thewisenerd/linters/sidekt/SideKtRuleSetProvider.kt
@@ -3,6 +3,7 @@ package io.github.thewisenerd.linters.sidekt
 import io.github.thewisenerd.linters.sidekt.rules.BlockingCallContext
 import io.github.thewisenerd.linters.sidekt.rules.BlockingCallContextReclaimable
 import io.github.thewisenerd.linters.sidekt.rules.JerseyMethodParameterDefaultValue
+import io.github.thewisenerd.linters.sidekt.rules.JerseyMissingHttpMethodAnnotation
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.api.RuleSet
 import io.gitlab.arturbosch.detekt.api.RuleSetProvider
@@ -15,7 +16,8 @@ class SideKtRuleSetProvider : RuleSetProvider {
         listOf(
             BlockingCallContext(config),
             BlockingCallContextReclaimable(config),
-            JerseyMethodParameterDefaultValue(config)
+            JerseyMethodParameterDefaultValue(config),
+            JerseyMissingHttpMethodAnnotation(config)
         )
     )
 }
